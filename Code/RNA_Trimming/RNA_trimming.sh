cac660cc851975175d6dffb1f5114b1238e4b953
Gabriel Hedman Slottner <gahe8576@rackham4.uppmax.uu.se>
2024-04-23 10:31:58 +0200

    Updated RNA_trimming script.

--- Code/RNA_Trimming/RNA_trimming.sh
+++ Code/RNA_Trimming/RNA_trimming.sh
@@ -3,9 +3,9 @@
 #SBATCH --reservation uppmax2024-2-7_7
 #SBATCH -M snowy
 #SBATCH -p core 
-#SBATCH -n 2
-#SBATCH -t 00:20:00
-#SBATCH -J Quality_analysis_test_RNA
+#SBATCH -n 4
+#SBATCH -t 04:00:00
+#SBATCH -J Trimming_RNA
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user gslottner@gmail.com
 
@@ -13,14 +13,11 @@ cat $0
 
 module load bioinfo-tools trimmomatic
 
-export SRCDIR=$HOME/Genome_analysis_repo/Data
-
-cp $SRCDIR/Raw_Data/RNA_untrimmed/RNA_untrimmed/* $SNIC_TMP
+export SRCDIR=$HOME/Genome_analysis_repo/Data/Raw_Data/RNA_untrimmed/RNA_untrimmed
 cd $SNIC_TMP
 
-for i in `ls $SRCDIR/Quality_analysis_before_trimming/RNA`
-do
-fastqc $i.fastq.gz
+java -jar trimmomatic-0.39.jar PE -trimlog Trimlog_SRR4342137.txt $SRCDIR/SRR4342137.1.fastq.gz $SRCDIR/SRR4342137.2.fastq.gz -baseout SRR4342137.fastq.gz
 
-cp ./${i}_* $SRCDIR/Quality_analysis_before_trimming/RNA/${i}/
-done
+java -jar trimmomatic-0.39.jar PE -trimlog Trimlog_SRR4342139.txt $SRCDIR/SRR4342139.1.fastq.gz $SRCDIR/SRR4342139.2.fastq.gz -baseout SRR4342139.fastq.gz
+
+cp ./* $HOME/Genome_analysis_repo/Data/Trimmed_RNA
