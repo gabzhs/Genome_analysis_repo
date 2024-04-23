729ef1344fbec04d4760500a1cd537f553be6364
Gabriel Hedman Slottner <gahe8576@rackham1.uppmax.uu.se>
2024-04-23 10:26:03 +0200

    Added post trimming analysis script and code used for interactive trimming.

---
+++ Code/Quality_analysis_after_trimming/RNA_Qual_post_trim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l 
+#SBATCH -A uppmax2024-2-7
+#SBATCH --reservation uppmax2024-2-7_7
+#SBATCH -M snowy
+#SBATCH -p core 
+#SBATCH -n 2
+#SBATCH -t 00:20:00
+#SBATCH -J Quality_analysis_test_RNA
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user gslottner@gmail.com
+
+cat $0
+
+module load bioinfo-tools FastQC
+
+export SRCDIR=$HOME/Genome_analysis_repo/Data
+
+cd $SNIC_TMP
+
+for i in `ls $SRCDIR/Trimmed_RNA`
+do
+fastqc $i
+
+done
+
+cp ./*.html $SRCDIR/Quality_analysis_after_trimming
+cp ./*.zip $SRCDIR/Quality_analysis_after_trimming
