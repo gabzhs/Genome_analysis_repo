e04ab96367bdd5b6d030ef83724d4715e0a53373
Gabriel Hedman Slottner <gahe8576@rackham3.uppmax.uu.se>
2024-04-24 02:17:35 +0200

    Changed time, reservation code and output locations of assembly script.

--- Code/Assembly/Assembly_script.sh
+++ Code/Assembly/Assembly_script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -l 
 #SBATCH -A uppmax2024-2-7
-#SBATCH --reservation uppmax2024-2-7_7
 #SBATCH -M snowy
 #SBATCH -p core 
 #SBATCH -n 4
-#SBATCH -t 08:00:00
+#SBATCH -t 16:00:00
 #SBATCH -J Metagenome_assembly
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user gslottner@gmail.com
@@ -15,4 +14,4 @@ module load bioinfo-tools megahit
 
 export SRCDIR=$HOME/Genome_analysis_repo/Data
 
-megahit -1 $SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz,$SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz -2 $SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz,$SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz -o $SRCDIR/Metagenome_assembly/Run_2
+megahit -1 $SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz,$SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz -2 $SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz,$SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz -o $SRCDIR/Metagenome_assembly/Run_3
