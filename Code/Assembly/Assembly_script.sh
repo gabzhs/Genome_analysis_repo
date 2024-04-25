38f6870c460d29fad9fda3ad27d9acf3c3267ab1
Gabriel Hedman Slottner <gahe8576@rackham1.uppmax.uu.se>
2024-04-25 14:11:40 +0200

    Added k settings, reduced cores.

--- Code/Assembly/Assembly_script.sh
+++ Code/Assembly/Assembly_script.sh
@@ -2,7 +2,7 @@
 #SBATCH -A uppmax2024-2-7
 #SBATCH -M snowy
 #SBATCH -p core 
-#SBATCH -n 4
+#SBATCH -n 2
 #SBATCH -t 08:00:00
 #SBATCH -J Metagenome_assembly
 #SBATCH --mail-type=ALL
@@ -14,4 +14,4 @@ module load bioinfo-tools megahit
 
 export SRCDIR=$HOME/Genome_analysis_repo/Data
 
-megahit --kmin-1pass -1 $SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz,$SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz -2 $SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz,$SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz -o $SRCDIR/Metagenome_assembly/Run_5
+megahit --kmin-1pass --k-min 59 --k-max 99 --k-step 10  -1 $SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz,$SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz -2 $SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz,$SRCDIR/Raw_Data/DNA_trimmed/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz -o $SRCDIR/Metagenome_assembly/Run_6
