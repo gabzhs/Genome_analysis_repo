0bb6863b4523d6dc356a988fa906eaf419d4cfaf
Gabriel Hedman Slottner <gahe8576@rackham3.uppmax.uu.se>
2024-04-26 13:03:27 +0200

    Updated Evaluation script.

---
+++ Code/Assembly_evaluation/Evaluation_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l 
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core 
+#SBATCH -n 2
+#SBATCH -t 01:30:00
+#SBATCH -J Assembly_evaluation
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user gslottner@gmail.com
+
+cat $0
+
+module load bioinfo-tools quast
+
+/sw/bioinfo/quast/5.0.2/snowy/bin/metaquast.py /home/gahe8576/Genome_analysis_repo/Data/Metagenome_assembly/Run_6/final.configs.fa -o /home/gahe8576/Genome_analysis_repo/Data/Assembly_evaluation/Run_2
