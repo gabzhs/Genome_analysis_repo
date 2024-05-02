c9d74404a7d9107f99a2381f5f8cc6395dcc6ca8
Gabriel Hedman Slottner <gahe8576@rackham1.uppmax.uu.se>
2024-05-02 15:07:26 +0200

    Wrote binning script.

---
+++ Code/Binning/Binning_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l 
+#SBATCH -A uppmax2024-2-7
+#SBATCH -M snowy
+#SBATCH -p core 
+#SBATCH -n 2
+#SBATCH -t 00:45:00
+#SBATCH -J Metagenome_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user gslottner@gmail.com
+
+cat $0
+
+module load bioinfo-tools MetaBat
+
+export SRCDIR=$HOME/Genome_analysis_repo/Data
+
+metabat -i SRCDIR/Metagenome_assembly/Run_6/final.contigs.fa -o SRCDIR/Bins/Run_1
