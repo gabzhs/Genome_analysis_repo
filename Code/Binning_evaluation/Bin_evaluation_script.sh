acc32050be506888c35ac672c3c204053d908100
Gabriel Hedman Slottner <gahe8576@rackham2.uppmax.uu.se>
2024-05-06 04:13:52 +0200

    Finished Bin evaluation script.

--- Code/Binning_evaluation/Bin_evaluation_script.sh
+++ Code/Binning_evaluation/Bin_evaluation_script.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core 
 #SBATCH -n 4
 #SBATCH -t 03:00:00
-#SBATCH -J Binning
+#SBATCH -J Binning_evaluation
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user gslottner@gmail.com
 
@@ -12,4 +12,6 @@ cat $0
 
 module load bioinfo-tools CheckM/1.1.3
 
+checkm data setRoot /home/gahe8576/Genome_analysis_repo/Data/Bin_evaluation/CheckM_data
 
+checkm lineage_wf -t 16 -x fa --reduced_tree /home/gahe8576/Genome_analysis_repo/Data/Bins/Run_1 /home/gahe8576/Genome_analysis_repo/Data/Bin_evaluation/Run_1
