c1aeb924c11e2bb9e524037933579708f47ff649
Gabriel Hedman Slottner <gahe8576@rackham2.uppmax.uu.se>
2024-05-08 03:31:08 +0200

    Changed to run two analyses.

--- Code/Phylogenetic_prediction/Phylogenetic_script.sh
+++ Code/Phylogenetic_prediction/Phylogenetic_script.sh
@@ -3,7 +3,7 @@
 #SBATCH -M snowy
 #SBATCH -p core 
 #SBATCH -n 4
-#SBATCH -t 08:00:00
+#SBATCH -t 09:00:00
 #SBATCH -J Prediction_of_phylogeny
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user gslottner@gmail.com
@@ -14,4 +14,6 @@ export CONDA_ENVS_PATH=/proj/uppmax2024-2-7/Genome_Analysis/conda_envs
 source conda_init.sh
 conda activate phylophlan
 
-phylophlan_assign_sgbs -i $HOME/Genome_analysis_repo/Data/Bins/Run_1 -o $HOME/Genome_analysis_repo/Data/Phylogenetic_prediction/Run_1 --nproc 4 -d SGB.Jan21 --database_folder /proj/uppmax2024-2-7/Genome_Analysis/conda_envs/SGB/phylophlan_databases -f /proj/uppmax2024-2-7/Genome_Analysis/conda_envs/configs
+phylophlan -i $HOME/Genome_analysis_repo/Data/Bins/Run_1 -o $HOME/Genome_analysis_repo o/Data/Phylogenetic_prediction/Run_1/Phylophlan --nproc 4 -d SGB.Jan21 --database_folder /proj/uppmax2024-2 -7/Genome_Analysis/conda_envs/SGB/phylophlan_databases -f /proj/uppmax2024-2-7/Genome_Analysis/conda_envs/configs
+
+phylophlan_assign_sgbs -i $HOME/Genome_analysis_repo/Data/Bins/Run_1 -o $HOME/Genome_analysis_repo/Data/Phylogenetic_prediction/Run_1/SGBs --nproc 4 -d SGB.Jan21 --database_folder /proj/uppmax2024-2-7/Genome_Analysis/conda_envs/SGB/phylophlan_databases
