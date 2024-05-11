002e5dd650a046c9987fd0e82faa666166224231
Gabriel Hedman Slottner <gahe8576@rackham3.uppmax.uu.se>
2024-05-11 05:07:41 +0200

    Attempted fix for file variable. Changed time.

--- Code/Functional_prediction/Func_pred_script.sh
+++ Code/Functional_prediction/Func_pred_script.sh
@@ -3,7 +3,7 @@
 #SBATCH -M snowy
 #SBATCH -p core 
 #SBATCH -n 4
-#SBATCH -t 09:00:00
+#SBATCH -t 02:00:00
 #SBATCH -J Prediction_of_function
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user gslottner@gmail.com
@@ -13,12 +13,12 @@ module load bioinfo-tools prokka
 input = /home/gahe8576/Genome_analysis_repo/Data/Bins/Good_bins_run_1
 output = /home/gahe8576/Genome_analysis_repo/Data/Functional_prediction/Run_1_good_bins
 
-for i in `ls $input/Bacteria`;
+for i in `ls ${input}/Bacteria`;
 do
-prokka --outdir $output/Bacteria --prefix $i $i;
+prokka --outdir ${output}/Bacteria --prefix $i $i;
 done
 
-for i in `ls $input/Archaea`;
+for i in `ls ${input}/Archaea`;
 do 
-prokka --kingdom Archaea --outdir $output/Archaea --prefix $i $i;
+prokka --kingdom Archaea --outdir ${output}/Archaea --prefix $i $i;
 done
