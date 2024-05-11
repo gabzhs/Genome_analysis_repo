78ce3eca19f3d54f28dffcc76e14ef4b38910bfe
Gabriel Hedman Slottner <gahe8576@rackham2.uppmax.uu.se>
2024-05-11 10:04:18 +0200

    Attempted fix to find files.

--- Code/Functional_prediction/Func_pred_script.sh
+++ Code/Functional_prediction/Func_pred_script.sh
@@ -10,10 +10,10 @@
 
 module load bioinfo-tools prokka
 
-export SRCDIR = $HOME/Genome_analysis_repo/Data
+export SRCDIR=$HOME/Genome_analysis_repo/Data
 
-export INPUT = $SRCDIR/Data/Bins/Good_bins_run_1
-export OUTPUT = $SRCDIR/Data/Functional_prediction/Run_1_good_bins
+export INPUT=$SRCDIR/Data/Bins/Good_bins_run_1
+export OUTPUT=$SRCDIR/Data/Functional_prediction/Run_1_good_bins
 
 for i in `ls $INPUT/Bacteria`;
 do
