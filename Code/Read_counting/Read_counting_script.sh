5151a1ec777a379ed760783e4d76d1c32bf9825a
Gabriel Hedman Slottner <gahe8576@rackham1.uppmax.uu.se>
2024-05-16 23:23:55 +0200

    Added fix to file paths.

--- Code/Read_counting/Read_counting_script.sh
+++ Code/Read_counting/Read_counting_script.sh
@@ -14,10 +14,10 @@ module load bioinfo-tools htseq
 
 export SRCDIR=/home/gahe8576/Genome_analysis_repo/Data
 
-cd $SRCDIR
+cd $SRCDIR/Bins/Best_bacterial_bins_run_1/
 
-for i in `ls $SRCDIR/Bins/Best_bacterial_bins_run_1/*.fa`
+for i in `ls ./*.fa`
 do
-htseq-count --format bam --order pos /Alignment/Run_2/$i.37.bam /Functional_prediction/Run_2_good_bins/Bacterial_features_without_nucleotides/$i.gff > /Counted_reads/Run_1/$i.37_counts.txt
-htseq-count --format bam --order pos /Alignment/Run_2/$i.39.bam /Functional_prediction/Run_2_good_bins/Bacterial_features_without_nucleotides/$i.gff > /Counted_reads/Run_1/$i.39_counts.txt
+htseq-count --format bam --order pos $SRCDIR/Alignment/Run_2/$i.37.bam $SRCDIR/Functional_prediction/Run_2_good_bins/Bacterial_features_without_nucleotides/$i.gff > $SRCDIR/Counted_reads/Run_1/$i.37_counts.txt
+htseq-count --format bam --order pos $SRCDIR/Alignment/Run_2/$i.39.bam $SRCDIR/Functional_prediction/Run_2_good_bins/Bacterial_features_without_nucleotides/$i.gff > $SRCDIR/Counted_reads/Run_1/$i.39_counts.txt
 done
